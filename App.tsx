6ca61e603f9a142d78650db43ba4c0d6e25bd2ef
Khadr <abkhadr@hotmail.com>
2025-09-10 15:37:40 +0300

    Mobile Commet

--- App.tsx
+++ App.tsx
@@ -1,45 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import "./global.css"
+import { Text, SafeAreaView as Div } from "react-native";
+import Svg, { Path } from 'react-native-svg';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <Div className="flex-1 items-center justify-center bg-white">
+      <Svg width="100" height="100" viewBox="0 0 100 100">
+        <Path d="M10,10 L90,10 L90,90 L10,90 Z" fill="blue" />
+      </Svg>
+      <Text className="text-xl font-bold text-blue-500">
+        توكلنا علي الله
+      </Text>
+    </Div>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
